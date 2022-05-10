8f114ef142b7f12b77493ded393d88674f604842
Erick Lopez <elopez@grupoasesores.com.mx>
2022-05-10 15:13:35 -0500

    se agrega decode Form MSON

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -1,8 +1,53 @@
 import React from 'react'
+import Component from "mson-react/lib/component";
+
 
 export const Form = (props) => {
+
+  console.log("ValueForm",props.val)
+
+  var resp = JSON.parse(props.val)
+  resp.push({
+    name: "submit",
+    component: "ButtonField",
+    type: "submit",
+    label: "Submit",
+    icon: "Save"
+  },
+  {
+    name: "reset",
+    component: "ButtonField",
+    label: "Reset",
+    icon: "Clear"
+  }) 
+
+  const definition = {
+    component: "Form",
+    fields: resp,
+    validators: []
+  };
+
   return (
-    <div dangerouslySetInnerHTML={{ __html: props.val }}>
-    </div>
+    /*<div dangerouslySetInnerHTML={{ __html: props.val }}>
+    </div>*/
+      <Component
+      definition={definition}
+      
+      // Clear the form
+      onReset={({ component }) => component.reset()}
+      onSubmit={({ component }) => {
+        // TODO: Contact some API with the data
+        //console.log("DATAJSON",definition)
+        //console.log("submitting", component.getValues());
+  
+        // Simulate response from API saying that email address is already in use and report this
+        // error to the user
+        if (component.get("fields.email.value") === "taken@example.com") {
+          component.set({ "fields.email.err": "already in use" });
+        } else {
+          // Everything was successful so redirect, show confirmation, etc...
+        }
+      }}
+    />
   )
 }
