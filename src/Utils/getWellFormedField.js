7518723d505d82192949f093184f7d0f92078aa8
Erick Lopez <elopez@grupoasesores.com.mx>
2022-05-26 11:29:11 -0500

    se agregan ajustes render forms

--- src/Utils/getWellFormedField.js
+++ src/Utils/getWellFormedField.js
@@ -1,6 +1,7 @@
 const TextFieldMSONAttributes = ["name", "component", "label", "required", "block", "fullWidth", "hidden", "disabled", "useDisplayValue", "multiline", "help", "minLength", "maxLength"];
 const SelectFieldMSONAttributes = ["name", "component", "label", "fullWidth", "options", "required", "block", "hidden", "disabled", "useDisplayValue", "help"];
 const BooleanFieldMSONAttributes = ["name", "label", "component", "required", "block", "fullWidth", "hidden", "disabled", "useDisplayValue"];
+const TextMSONAttributes = ["name", "component", "text"];
 
 export const getWellFormedField = (fieldItem) => {
     let component = {}
@@ -12,6 +13,9 @@ export const getWellFormedField = (fieldItem) => {
             component[property] = fieldItem[property]
         }
         if (fieldItem.component === "BooleanField" && BooleanFieldMSONAttributes.includes(property)) {
+            component[property] = fieldItem[property]
+        }
+        if (fieldItem.component === "Text" && TextMSONAttributes.includes(property)){
             component[property] = fieldItem[property]
         }
     }
